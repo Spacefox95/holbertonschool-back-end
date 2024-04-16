ffe43a1dfb447689a6c0acd670750bcb78c91017
Spacefox95 <nathan.raynal@gmail.com>
2024-04-16 12:45:03 +0200

    ok

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -6,31 +6,33 @@ This script export data in Json format :
     _ Task_completed_stats (new_task_status)
     _ Task_title (new_task_title)
 """
-import requests
-import json
+
 from sys import argv
+import json
+import requests
 
 
 def Print_Employee_TODO_LIST(employee_id):
     """ Return API data """
     employee_name_request = requests.get(
         f'https://jsonplaceholder.typicode.com/users/{employee_id}')
-    username = employee_name_request.json()['username']
+    USERNAME = employee_name_request.json()['username']
+    USER_ID = employee_id
     response_API = requests.get(
         f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
     todolist = response_API.json()
     with open("{}.json".format(employee_id), 'w') as file:
         tasks_list = []
         for new_task in todolist:
-            new_task_title = new_task['title']
-            new_task_status = new_task['completed']
+            TASK_TITLE = new_task['title']
+            TASK_COMPLETED_STATUS = new_task['completed']
             task_data = {
-                "task": new_task_title,
-                "completed": new_task_status,
-                "username": username
+                "task": TASK_TITLE,
+                "completed": TASK_COMPLETED_STATUS,
+                "username": USERNAME
                 }
             tasks_list.append(task_data)
-        json.dump({str(employee_id): tasks_list}, file)
+        json.dump({str(USER_ID): tasks_list}, file)
 
 
 if __name__ == "__main__":
